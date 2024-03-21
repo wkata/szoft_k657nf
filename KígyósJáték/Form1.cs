d9c3f3eb05c6d729b94ddd2ecd871607232f207c
wkata <kata.wagner@stud.uni-corvinus.hu>
2024-03-21 12:51:44 +0100

    kígyócska

---
+++ KígyósJáték/Form1.cs
@@ -0,0 +1,90 @@
+namespace KígyósJáték
+{
+    public partial class Form1 : Form
+    {
+        int fej_x = 100;
+        int fej_y = 100;
+        int irany_x = 1;
+        int irany_y = 0;
+        int lépésszám;
+        List<KígyóElem> kígyó = new();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lépésszám++;
+            
+
+            fej_x += irany_x * KígyóElem.Méret;
+            fej_y += irany_y * KígyóElem.Méret;
+
+            foreach (object item in Controls)
+            {
+                if (item is KígyóElem)
+                {
+                    KígyóElem k = (KígyóElem)item;
+
+                    if (k.Top == fej_y && k.Left == fej_x)
+                    { timer1.Enabled = false; return; }
+                }
+            }
+
+            
+
+           
+            KígyóElem ke = new KígyóElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+            kígyó.Add(ke);
+            Controls.Add(ke);
+
+            if (kígyó.Count > KígyóElem.hossz) 
+            {
+                KígyóElem levágandó = kígyó[0];
+                kígyó.RemoveAt(0);
+                Controls.Remove(levágandó);
+            }
+
+            if (lépésszám % 2 == 0) ke.BackColor = Color.GreenYellow;
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) 
+            {
+                irany_y = -1;
+                irany_x = 0;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                irany_y = 1;
+                irany_x = 0;
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                irany_x = -1;
+                irany_y = 0;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                irany_x = 1;
+                irany_y = 0;
+            }
+            
+
+        }
+
+        
+    }
+}
---
+++ KígyósJáték/KígyóElem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KígyósJáték
+{
+    internal class KígyóElem : PictureBox
+    {
+        public static int Méret=20;
+        public static int hossz = 8;
+        public KígyóElem()
+        {
+            Width = KígyóElem.Méret;
+            Height = KígyóElem.Méret;
+            BackColor = Color.Fuchsia;
+
+
+        }
+    }
+}
